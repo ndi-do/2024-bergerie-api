04a0d6aa0cd6054084ff02d6ad2c10258761566b
nathaelbonnal <pro.nathaelbonnal@gmail.com>
2024-12-06 02:30:36 +0100

    chore: Update helm chart path and remove unused file

--- start/env.ts
+++ start/env.ts
@@ -23,9 +23,9 @@ export default await Env.create(new URL('../', import.meta.url), {
   | Variables for configuring database connection
   |----------------------------------------------------------
   */
-  DB_HOST: Env.schema.string({ format: 'host' }),
+  DB_HOST: Env.schema.string(),
   DB_PORT: Env.schema.number(),
   DB_USER: Env.schema.string(),
   DB_PASSWORD: Env.schema.string.optional(),
-  DB_DATABASE: Env.schema.string()
+  DB_DATABASE: Env.schema.string(),
 })
